7e0ea4920b2124f0bef3af070d23074141034241
d-nezasa <47309212+d-nezasa@users.noreply.github.com>
2019-04-14 21:53:42 +0900

    Create 3min02.html/js

--- JavaScript/3minStudy/3min02.js
+++ JavaScript/3minStudy/3min02.js
@@ -1,3 +1,24 @@
-var count = 3;
-var sumh = 164 + 172 + 182;
-alert(((sumh/count).toString()).substring(0, 5));
+var count = 0;
+var sumh = 0;
+var nam = document.getElementById("n");
+var height = document.getElementById("h");
+var table = document.getElementById("result");
+var avgh = document.getElementById("avgh")
+
+function showResult(){
+    count++;
+    sumh += parseFloat(height.value);
+    var tr = document.createElement("tr");
+    var tdl = document.createElement("td");
+    tdl.id = "n" + count;
+    tdl.appendChild(document.createTextNode(nam.value));
+    var td2 = document.createElement("td");
+    td2.id = "h" + count;
+    td2.appendChild(document.createTextNode(height.value + "cm"));
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    table.appendChild(tr);
+    avgh.innerHTML=((sumh/count).toString()).substring(0, 5);
+    nam.value="";
+    height.value="";
+}
